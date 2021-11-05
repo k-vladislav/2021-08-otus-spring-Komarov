56461ed549d7bce3a7b4b786b46bd608ac6a81d5
k-vladislav <v.kom91@gmail.com>
2021-11-05 17:32:03 +0300

    User - убрал статический фабричный метод

--- Homework-3/src/main/java/ru/otus/spring/domain/User.java
+++ Homework-3/src/main/java/ru/otus/spring/domain/User.java
@@ -3,13 +3,9 @@ package ru.otus.spring.domain;
 public class User {
     private final String firstName, lastName;
 
-    private User(String firstName, String lastName) {
+    public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-    }
-
-    public static User create(String firstName, String lastName) {
-        return new User(firstName, lastName);
     }
 
     @Override
