c7d126beb05f6b11b3961e2a7a21eaf7becc5d2c
k-vladislav <v.kom91@gmail.com>
2021-11-06 20:46:33 +0300

    QuizServiceCSVTest.java //todo

--- Homework-3/src/test/java/ru/otus/spring/Homework3ApplicationTests.java
+++ Homework-3/src/test/java/ru/otus/spring/Homework3ApplicationTests.java
@@ -1,11 +1,11 @@
 package ru.otus.spring;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class Homework3ApplicationTests {
     @Test
     void contextLoads() {
--- Homework-3/src/test/java/ru/otus/spring/service/QuizServiceCSVTest.java
+++ Homework-3/src/test/java/ru/otus/spring/service/QuizServiceCSVTest.java
@@ -1,20 +1,45 @@
 package ru.otus.spring.service;
 
 import org.apache.commons.csv.CSVRecord;
+
+import org.assertj.core.api.*;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestComponent;
-import ru.otus.spring.dao.QuizDaoCSV;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Collections;
+import java.util.List;
 
+
+@SpringBootTest
 @TestComponent
 class QuizServiceCSVTest {
+    @Configuration
+     static class QuizServiceTestConfiguration {
+
+    }
+
+    @MockBean
+    QuizService<CSVRecord> quizService;
+
+    @MockBean
+    List<CSVRecord> csvRecords;
 
     @Test
+    void shouldReturnListOfCSVRecords() { //todo что я тут проверяю
+        //Mockito.when(quizService.getQuizRawData()).thenReturn(Collections.emptyList());
+        Mockito.when(quizService.getQuizRawData()).thenReturn(csvRecords);
+        Assertions.assertThat(quizService.getQuizRawData()).isEqualTo(csvRecords);
+    }
+
+/*    @Test
     void shouldCatchException() {
         String sourcePath = "/wrong/path";
         QuizDaoCSV dao = new QuizDaoCSV(sourcePath);
         QuizService<CSVRecord> mockQuizService = new QuizServiceCSV(dao);
         assertThrows(Exception.class, mockQuizService::getQuizRawData);
-    }
+    }*/
 }
